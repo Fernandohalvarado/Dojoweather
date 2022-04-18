dd6c33aae8d241d5c1f0bdc4b5467b6c6eb29859
Fernandohalvarado <100388323+Fernandohalvarado@users.noreply.github.com>
2022-04-18 17:08:01 -0500

    intitial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var acceptbtn = document.querySelector(".cookie")
+function hide(Element){
+    acceptbtn.remove();
+}
+var change1 = document.querySelector("#high1")
+function convert(){
+    change1.innerText = "75°";
+}
+var change2 = document.querySelector("#low1")
+function convert(){
+    change2.innerText = "65°";
+}
